d1cc0ecdf2f474648598a7a26008b88798259c73
daidai jie <317095153@qq.com>
2016-03-06 21:19:01 +0800

    add a login activity

---
+++ app/src/main/java/com/hjsmallfly/syllabus/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.hjsmallfly.syllabus.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hjsmallfly.syllabus.syllabus.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activity);
+    }
+}
