98f8447de80ffbba433317e18a8b2518095d2ebc
hjsmallfly <szu_nwad@icloud.com>
2016-09-09 12:21:12 +0800

    use new Internet Login System

--- app/src/main/java/com/hjsmallfly/syllabus/helpers/InternetLogin.java
+++ app/src/main/java/com/hjsmallfly/syllabus/helpers/InternetLogin.java
@@ -5,6 +5,9 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 
 /**
@@ -12,14 +15,11 @@ import java.util.HashMap;
  * 用于连接到校园网
  */
 public class InternetLogin {
-//    request_url = "http://192.168.31.4:8080/?url="
-//    postdata = urllib.parse.urlencode({
-//        "AuthenticateUser": user,
-//                "AuthenticatePassword": passwd,
-//                "Submit": ""
-//    })
 
-    public static String REQUEST_URL = "http://192.168.31.4:8080/?url=";
+    /**
+     * 用于登录和注销的API地址
+     */
+    public static String REQUEST_URL = "http://1.1.1.2/ac_portal/login.php";
 
     private Context context;
     private String username;
@@ -41,52 +41,30 @@ public class InternetLogin {
     private class LoginTask extends AsyncTask<Void, Void, String>{
 
 
+
         @Override
         protected String doInBackground(Void... params) {
             HashMap<String, String> post_data = new HashMap<>();
-            post_data.put("AuthenticateUser", InternetLogin.this.username);
-            post_data.put("AuthenticatePassword", InternetLogin.this.password);
-            post_data.put("Submit", "");
+            post_data.put("opr", "pwdLogin");
+            post_data.put("userName", InternetLogin.this.username);
+            post_data.put("pwd", InternetLogin.this.password);
+            post_data.put("rememberPwd", "0");
 
             return HttpCommunication.performPostCall(InternetLogin.REQUEST_URL, post_data);
         }
 
         @Override
         protected void onPostExecute(String s) {
-//            Log.d("inet", "response: " + s);
-            if (s.isEmpty()){
-                Toast.makeText(InternetLogin.this.context, "请确保接入校园网以及密码和账号的匹配", Toast.LENGTH_SHORT).show();
-                return;
+            Log.d("newFlux", s);
+            try {
+                JSONObject loginObject = new JSONObject(s);
+//                boolean success = loginObject.getBoolean("success");
+                String msg = loginObject.getString("msg");
+                Toast.makeText(InternetLogin.this.context, msg, Toast.LENGTH_SHORT).show();
+            } catch (JSONException e) {
+                Toast.makeText(InternetLogin.this.context, "网络连接错误", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
             }
-
-            String lower_case = s.toLowerCase();
-            if (lower_case.contains("used bytes")){
-//                Log.d("inet", "已接入外网");
-                // 截取数据
-                String begin = "id=\"ub\"";
-                String end = "</td>";
-                String used_bytes_string = null;
-                int begin_index = lower_case.indexOf(begin);
-                if (begin_index > 0){
-                    int end_index = lower_case.indexOf(end, begin_index);
-                    if (end_index > 0){
-                        used_bytes_string = lower_case.substring(begin_index + begin.length() + 1, end_index);
-                        used_bytes_string = used_bytes_string.replace(",", "");
-                    }
-                }
-
-                int used_mb = -1;
-                if (used_bytes_string != null){
-                    used_mb = Integer.parseInt(used_bytes_string) / (1024 * 1024);
-                }
-
-                if (used_mb > 0)
-                    Toast.makeText(InternetLogin.this.context, "已接入外网 已使用流量" + used_mb + "MB", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(InternetLogin.this.context, "已接入外网 " + used_bytes_string, Toast.LENGTH_SHORT).show();
-                return;
-            }
-
         }
     }
 
